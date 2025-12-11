26071a01cc564281c2e60dfc0a74bef4cc78a2c2
azmogo <arthurwallois2@gmail.com>
2025-12-11 20:30:39 +0100

    add rc4 cipher

--- include/actions.h
+++ include/actions.h
@@ -43,5 +43,14 @@ int action_caesar(uint8_t *data, size_t size, int shift);
  */
 int action_xor(uint8_t *data, size_t size, const char *key);
 
+/**
+ * @brief apply rc4 cipher
+ * @param data data buffer
+ * @param size buffer size
+ * @param key encryption key (null-terminated string)
+ * @return 0 on success, -1 on error
+ */
+int action_rc4(uint8_t *data, size_t size, const char *key);
+
 #endif /* ACTIONS_H */
 
--- src/actions.c
+++ src/actions.c
@@ -54,3 +54,45 @@ int action_xor(uint8_t *data, size_t size, const char *key) {
     return 0;
 }
 
+/* rc4 key scheduling */
+static void rc4_ksa(const char *key, size_t key_len, uint8_t *S) {
+    for (int i = 0; i < 256; i++) {
+        S[i] = (uint8_t)i;
+    }
+
+    int j = 0;
+    for (int i = 0; i < 256; i++) {
+        j = (j + S[i] + (uint8_t)key[i % key_len]) % 256;
+        uint8_t temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+    }
+}
+
+/* rc4 prga */
+static uint8_t rc4_prga(uint8_t *S, int *i, int *j) {
+    *i = (*i + 1) % 256;
+    *j = (*j + S[*i]) % 256;
+    uint8_t temp = S[*i];
+    S[*i] = S[*j];
+    S[*j] = temp;
+    int t = (S[*i] + S[*j]) % 256;
+    return S[t];
+}
+
+int action_rc4(uint8_t *data, size_t size, const char *key) {
+    if (!key || strlen(key) == 0) {
+        return -1;
+    }
+    size_t key_len = strlen(key);
+    uint8_t S[256];
+    int i = 0, j = 0;
+
+    rc4_ksa(key, key_len, S);
+    for (size_t k = 0; k < size; k++) {
+        uint8_t keystream_byte = rc4_prga(S, &i, &j);
+        data[k] ^= keystream_byte;
+    }
+    return 0;
+}
+
