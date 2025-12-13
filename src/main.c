c2cbf9a9a2ba4845ba8d60cbaf1b08b0b01e5820
azmogo <arthurwallois2@gmail.com>
2025-12-13 12:39:15 +0100

    complete main with argument parsing and execution

--- src/main.c
+++ src/main.c
@@ -3,7 +3,24 @@
  * @brief main entry point for bitkitchen tool
  */
 
+#include "../include/bitkitchen.h"
+#include "../include/processor.h"
+
 int main(int argc, char *argv[]) {
-    return 0;
+    config_t config;
+    int result = parse_arguments(argc, argv, &config);
+    
+    if (result == -2) {
+        /* help was printed */
+        return 0;
+    }
+    if (result < 0) {
+        return 1;
+    }
+    
+    result = execute_config(&config);
+    free_config(&config);
+    
+    return result;
 }
 
